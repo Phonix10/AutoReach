581cb7059047618a98c83b5dd7c9531ab72e3a51
Udit Ranjan <101802436+Phonix10@users.noreply.github.com>
2025-10-14 02:44:48 +0530

    added file read code

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import pandas as pd
+import yagmail
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# --- STEP 1: Load Excel sheet ---
+file_path = r"C:\Users\uditr\Downloads\_1800+ Talent Acquisition Database .xlsx"  # Use raw string (r"...")
+df = pd.read_excel(file_path)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(df.sample(5))
